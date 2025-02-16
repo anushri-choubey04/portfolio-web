cb39b0ef44d7111018621ed07e22f100e16dfb5c
anushri-choubey04 <anushrichoubey04@gmail.com>
2025-02-16 17:06:21 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,34 @@
-/**
- * Application component
- *
- * To contain application wide settings, routes, state, etc.
- */
-
 import React from "react";
+
+import About from "./Components/About";
+import Footer from "./Components/Footer";
 import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Portfolio from "./Components/Portfolio";
+
+import "./styles.css";
+
+const siteProps = {
+  name: "Anushri Choubey",
+  title: "Web Developer",
+  email: "anushrichoubey04@gmail.com.com",
+  gitHub: "microsoft",
+  instagram: "microsoft",
+  youTube: "Code",
+};
+
+const primaryColor = "#4E567E";
+const secondaryColor = "#D2F1E4";
 
 const App = () => {
   return (
-    <>
+    <div id="main">
       <Header />
-      <h3>Built using Codespaces! 🥳</h3>
-      <p>Follow instructions in README to utilize this template for your site or class.</p>
-    </>
+      <Home name={siteProps.name} title={siteProps.title} />
+      <About />
+      <Portfolio />
+      <Footer {...siteProps} primaryColor={primaryColor} secondaryColor={secondaryColor} />
+    </div>
   );
 };
 
